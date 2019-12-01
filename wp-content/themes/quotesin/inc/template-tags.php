a34feb84e2363e0c9fed8cf3bd8d2f237d1319e6
Fahad Murtaza <fahdim@gmail.com>
2019-12-01 05:01:34 +0500

    tag template cleanup

--- wp-content/themes/quotesin/inc/template-tags.php
+++ wp-content/themes/quotesin/inc/template-tags.php
@@ -2,80 +2,78 @@
 /**
  * Custom template tags for this theme
  *
- * Eventually, some of the functionality here could be replaced by core features.
+ * Eventually, some of the functionality here could be replaced by core
+ * features.
  *
  * @package QuotesIn
  */
 
-if ( !function_exists( 'quotesin_paging_nav' ) ) :
+if ( ! function_exists( 'quotesin_paging_nav' ) ) :
 	/**
 	 * Display navigation to next/previous set of posts when applicable.
 	 */
-	function quotesin_paging_nav()
-	{
+	function quotesin_paging_nav() {
 		// Don't print empty markup if there's only one page.
 		if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
 			return;
 		}
 		?>
-		<nav class="navigation paging-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'quotesin' ); ?></h1>
+        <nav class="navigation paging-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php esc_html_e( 'Posts navigation', 'quotesin' ); ?></h1>
 
-			<div class="navi-links">
+            <div class="navi-links">
 
 				<?php if ( get_next_posts_link() ) : ?>
-					<div
-						class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'quotesin' ) ); ?></div>
+                    <div
+                            class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'quotesin' ) ); ?></div>
 				<?php endif; ?>
 
 				<?php if ( get_previous_posts_link() ) : ?>
-					<div
-						class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'quotesin' ) ); ?></div>
+                    <div
+                            class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'quotesin' ) ); ?></div>
 				<?php endif; ?>
 
-			</div>
-			<!-- .navi-links -->
-		</nav><!-- .navigation -->
-	<?php
+            </div>
+            <!-- .navi-links -->
+        </nav><!-- .navigation -->
+		<?php
 	}
 endif;
 
-if ( !function_exists( 'quotesin_post_nav' ) ) :
+if ( ! function_exists( 'quotesin_post_nav' ) ) :
 	/**
 	 * Display navigation to next/previous post when applicable.
 	 */
-	function quotesin_post_nav()
-	{
+	function quotesin_post_nav() {
 		// Don't print empty markup if there's nowhere to navigate.
 		$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-		$next = get_adjacent_post( false, '', false );
+		$next     = get_adjacent_post( false, '', false );
 
-		if ( !$next && !$previous ) {
+		if ( ! $next && ! $previous ) {
 			return;
 		}
 		?>
-		<nav class="navigation post-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'quotesin' ); ?></h1>
+        <nav class="navigation post-navigation" role="navigation">
+            <h1 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'quotesin' ); ?></h1>
 
-			<div class="navi-links">
+            <div class="navi-links">
 				<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'quotesin' ) );
 				next_post_link( '<div class="nav-next">%link</div>', _x( '%title&nbsp;<span class="meta-nav">&rarr;</span>', 'Next post link', 'quotesin' ) );
 				?>
-			</div>
-			<!-- .navi-links -->
-		</nav><!-- .navigation -->
-		<div class="clear"></div>
-	<?php
+            </div>
+            <!-- .navi-links -->
+        </nav><!-- .navigation -->
+        <div class="clear"></div>
+		<?php
 	}
 endif;
 
-if ( !function_exists( 'quotesin_posted_on' ) ) :
+if ( ! function_exists( 'quotesin_posted_on' ) ) :
 	/**
 	 * Prints HTML with meta information for the current post-date/time and author.
 	 */
-	function quotesin_posted_on()
-	{
+	function quotesin_posted_on() {
 		$quotesin_time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 
 		$quotesin_time_string = sprintf( $quotesin_time_string,
@@ -86,7 +84,7 @@ if ( !function_exists( 'quotesin_posted_on' ) ) :
 		);
 
 		$quotesin_posted_on = sprintf(
-			/* translators: %s: Date. */
+		/* translators: %s: Date. */
 			_x( 'Date: %s', 'post date', 'quotesin' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $quotesin_time_string . '</a>'
 		);
@@ -101,17 +99,16 @@ endif;
  *
  * @return bool
  */
-function quotesin_categorized_blog()
-{
+function quotesin_categorized_blog() {
 	if ( false === ( $all_the_cool_cats = get_transient( 'quotesin_categories' ) ) ) {
 		// Create an array of all the categories that are attached to posts.
-		$all_the_cool_cats = get_categories( array(
+		$all_the_cool_cats = get_categories( [
 			'fields'     => 'ids',
 			'hide_empty' => 1,
 
 			// We only need to know if there is more than one category.
 			'number'     => 2,
-		) );
+		] );
 
 		// Count the number of categories that are attached to the posts.
 		$all_the_cool_cats = count( $all_the_cool_cats );
@@ -132,8 +129,7 @@ function quotesin_categorized_blog()
 /**
  * Flush out the transients used in quotesin_categorized_blog.
  */
-function quotesin_category_transient_flusher()
-{
+function quotesin_category_transient_flusher() {
 	// Like, beat it. Dig?
 	delete_transient( 'quotesin_categories' );
 }
